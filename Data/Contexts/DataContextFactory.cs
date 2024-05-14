3a786fbe8469a53caf9565b18728723786ffc9e1
Anna Özmehak <anna.ozmehak@gmail.com>
2024-05-14 12:05:28 +0200

    newsletter provider done

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:sqldatabase-silicon-win23-annaozmehak.database.windows.net,1433;Initial Catalog=silicon-database;Persist Security Info=False;User ID=SqlAdmin;Password=Hebbe100!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
